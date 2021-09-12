9cefb3704c0dee6fdd1d484f1c54a4f771126f31
marjan <m.amiri.js@gmail.com>
2021-09-12 13:36:04 +0430

    commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var res = porampt("Enter Number");
-var res2 = porampt("Enter Number2");
+var res = prampt("Enter Number");
+var res2 = prampt("Enter Number2");
 var Num1 = parsInt(res);
 var Num2 = parsInt(res2);
 var sum = Num1 + Num2
