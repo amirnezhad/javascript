9177b1fb3144e58af36627a52fe33534f0f325eb
marjan <m.amiri.js@gmail.com>
2021-09-13 17:00:52 +0430

    add a function for sub

--- main.js
+++ main.js
@@ -7,9 +7,36 @@ function add(leftOperand, rightOperand) {
     return firstInput + secondInput
 
 }
-// start:
-let res = prompt("Enter first operand:\t");
-let res2 = prompt("Enter second operand:\t");
 
-let result = add(res, res2);
-console.log(result)
+function sub(leftOperand, rightOperand) {
+    let firstInput = parseInt(leftOperand);
+    let secondInput = parseInt(rightOperand);
+    //   let result = firstInput+ secondInput
+    return firstInput - secondInput
+
+}
+// start:
+let operation;
+let question = prompt("+ or -?\t")
+switch (question) {
+    case "+":
+        operation = add;
+        break;
+
+    case "-":
+        operation = sub;
+        break;
+
+    default:
+        console.log('error')
+
+
+}
+if (operation) {
+    let res = prompt("Enter first operand:\t");
+    let res2 = prompt("Enter second operand:\t");
+
+    let result = operation(res, res2);
+    console.log(result)
+}
+console.log('bye')
